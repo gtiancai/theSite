ad3007e766f3a839ef3c834712a1a880efbb31bb
gtic <gtianshu@163.com>
2018-01-31 14:46:59 +0800

    To support rich editor to create/editor rich text.

--- theSite/Controllers/NewsController.cs
+++ theSite/Controllers/NewsController.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using theSite.Models;
+using theSite.ViewModels;
 
 namespace theSite.Controllers
 {
@@ -23,14 +24,16 @@ namespace theSite.Controllers
 
         public ActionResult List()
         {
-            // var news = db.News.Include(n => n.User);
-            // return View(news.ToList());
+            var news = db.News.Include(n => n.User);
+            return View(news.ToList());
+            /*
             var news = new News() { ID = 2, CreatedBy = 1, CreatedDateTime = System.DateTime.Now, Detail = "hehe", ModifiedDateTime = DateTime.Now, Summary = "hello", Title = "abc", User = null };
             IList<News> newsList = new List<News>();
 
             newsList.Add(news);
 
             return View(newsList);
+            */
         }
 
         // GET: News/Details/5
@@ -41,17 +44,22 @@ namespace theSite.Controllers
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
             News news = db.News.Find(id);
+
             if (news == null)
             {
                 return HttpNotFound();
             }
-            return View(news);
+            HtmlDecode(news);
+
+            NewsViewModel newsVM = CreateNewsVMFromNews(news);
+
+            return View(newsVM);
         }
 
         // GET: News/Create
         public ActionResult Create()
         {
-            ViewBag.CreatedBy = new SelectList(db.Users, "ID", "Name");
+            // ViewBag.CreatedBy = new SelectList(db.Users, "ID", "Name");
             return View();
         }
 
@@ -60,17 +68,29 @@ namespace theSite.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ID,Title,Summary,Detail,CreatedBy,CreatedDateTime,ModifiedDateTime")] News news)
+        [ValidateInput(false)]
+        // public ActionResult Create([Bind(Include = "ID,Title,Summary,Detail,CreatedBy,CreatedDateTime,ModifiedDateTime")] News news)
+        public ActionResult Create([Bind(Include = "Title, Summary, Detail")] NewsViewModel newsVM)
         {
             if (ModelState.IsValid)
             {
+                /*
+                News n = CreateCopy2Save(news);
+                */
+
+                News news = CreateNewsFromNewsVM(newsVM);
+                news.CreatedDateTime = DateTime.UtcNow;
+
+                HtmlEncode(news);
+
                 db.News.Add(news);
                 db.SaveChanges();
+
                 return RedirectToAction("Index");
             }
 
-            ViewBag.CreatedBy = new SelectList(db.Users, "ID", "Name", news.CreatedBy);
-            return View(news);
+            // ViewBag.CreatedBy = new SelectList(db.Users, "ID", "Name", news.CreatedBy);
+            return View(newsVM);
         }
 
         // GET: News/Edit/5
@@ -80,13 +100,21 @@ namespace theSite.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
+
             News news = db.News.Find(id);
+
             if (news == null)
             {
                 return HttpNotFound();
             }
-            ViewBag.CreatedBy = new SelectList(db.Users, "ID", "Name", news.CreatedBy);
-            return View(news);
+
+            HtmlDecode(news);
+
+
+            NewsViewModel newsVM = CreateNewsVMFromNews(news);
+
+            // ViewBag.CreatedBy = new SelectList(db.Users, "ID", "Name", news.CreatedBy);
+            return View(newsVM);
         }
 
         // POST: News/Edit/5
@@ -94,16 +122,30 @@ namespace theSite.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,Title,Summary,Detail,CreatedBy,CreatedDateTime,ModifiedDateTime")] News news)
+        [ValidateInput(false)]
+        // public ActionResult Edit([Bind(Include = "ID,Title,Summary,Detail,CreatedBy,CreatedDateTime,ModifiedDateTime")] News news)
+        public ActionResult Edit([Bind(Include = "ID, Title, Summary, Detail")] NewsViewModel newsVM)
         {
             if (ModelState.IsValid)
             {
+                // News n = CreateCopy2Save(news);
+                // News news = CreateNewsFromNewsVM(newsVM);
+                News news = db.News.Find(newsVM.ID);
+
+                news.Title = newsVM.Title;
+                news.Summary = newsVM.Summary;
+                news.Detail = newsVM.Detail;
+                news.ModifiedDateTime = DateTime.UtcNow;
+
+                HtmlEncode(news);
+
                 db.Entry(news).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            ViewBag.CreatedBy = new SelectList(db.Users, "ID", "Name", news.CreatedBy);
-            return View(news);
+
+            // ViewBag.CreatedBy = new SelectList(db.Users, "ID", "Name", news.CreatedBy);
+            return View(newsVM);
         }
 
         // GET: News/Delete/5
@@ -139,6 +181,68 @@ namespace theSite.Controllers
                 db.Dispose();
             }
             base.Dispose(disposing);
+        }
+
+        private News CreateCopy2Save(News news)
+        {
+            News n = new News()
+            {
+                ID = news.ID,
+                CreatedDateTime = DateTime.UtcNow,
+                ModifiedDateTime = DateTime.UtcNow
+            };
+
+            string userName = System.Web.HttpContext.Current.User.Identity.Name;
+
+            User user = db.Users.Where(u => u.Name.Equals(userName)).FirstOrDefault();
+            n.CreatedBy = user != null ? user.ID : 0;
+
+            HtmlEncode(n);
+
+            return n;
+        }
+
+        private void HtmlEncode(News news)
+        {
+            news.Title = HttpUtility.HtmlEncode(news.Title);
+            news.Summary = HttpUtility.HtmlEncode(news.Summary);
+            news.Detail = HttpUtility.HtmlEncode(news.Detail);
+        }
+
+        private void HtmlDecode(News news)
+        {
+            news.Title = HttpUtility.HtmlDecode(news.Title);
+            news.Summary = HttpUtility.HtmlDecode(news.Summary);
+            news.Detail = HttpUtility.HtmlDecode(news.Detail);
+        }
+
+        private NewsViewModel CreateNewsVMFromNews(News news)
+        {
+            NewsViewModel newsVM = new NewsViewModel()
+            {
+                ID = news.ID,
+                Title = news.Title,
+                Summary = news.Summary,
+                Detail = news.Detail
+            };
+
+            return newsVM;
+        }
+
+        private News CreateNewsFromNewsVM(NewsViewModel newsVM)
+        {
+            News news = new News()
+            {
+                Title = newsVM.Title,
+                Summary = newsVM.Summary,
+                Detail = newsVM.Detail,
+                // CreatedDateTime = ; // Not change it if do update.
+                ModifiedDateTime = DateTime.UtcNow
+
+                , CreatedBy = 1 // remove?
+            };
+
+            return news;
         }
     }
 }
---
+++ theSite/ViewModels/NewsViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace theSite.ViewModels
+{
+    public class NewsViewModel
+    {
+        [Display(Name = "No.")]
+        public long ID { get; set; }
+
+        [Display(Name = "Title")]
+        public string Title { get; set; }
+
+        [Display(Name = "Summary")]
+        public string Summary { get; set; }
+
+        [Display(Name = "Content")]
+        [DataType(DataType.MultilineText)]
+        public string Detail { get; set; }
+
+        /*
+        public int CreatedBy { get; set; }
+        public Nullable<System.DateTime> CreatedDateTime { get; set; }
+        public Nullable<System.DateTime> ModifiedDateTime { get; set; }
+
+        public virtual User User { get; set; }
+        */
+    }
+}
